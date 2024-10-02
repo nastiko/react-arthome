711623a50ff7b21c03617690a6ebb81bfa526569
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-10-02 22:26:36 +0100

    get data "media" from REST API, added content on the Post page and styled it, still working on Post page

--- src/api.js
+++ src/api.js
@@ -3,12 +3,11 @@ export async function getSlides() {
     try {
         const request = await fetch("https://ecommerce.anastasia-web.dev/wp-json/wp/v2/slides");
 
-        if(!request.ok) {
+        if (!request.ok) {
             throw new Error(`HTTP error! Status: ${request.status}`);
         }
         return await request.json();
-    }
-    catch(error) {
+    } catch (error) {
         console.error('Failed to fetch slides:', error);
     }
 }
@@ -18,12 +17,25 @@ export async function getPosts(id) {
     try {
         const url = id ? `https://ecommerce.anastasia-web.dev/wp-json/wp/v2/posts/${id}` : "https://ecommerce.anastasia-web.dev/wp-json/wp/v2/posts";
         const request = await fetch(url);
-        if(!request.ok) {
+        if (!request.ok) {
             throw new Error(`HTTP error! Status: ${request.status}`);
         }
         return await request.json();
-    }
-    catch(error) {
+    } catch (error) {
         console.error('Failed to fetch slides:', error);
+    }
+}
+
+// api media
+export async function getMedia(id) {
+    try {
+        const url = id ? `https://ecommerce.anastasia-web.dev/wp-json/wp/v2/media/${id}` : "https://ecommerce.anastasia-web.dev/wp-json/wp/v2/media";
+        const request = await fetch(url);
+        if (!request.ok) {
+            throw new Error(`HTTP error! Status: ${request.status}`);
+        }
+        return await request.json();
+    } catch {
+        console.error('Failed to fetch slides:', id);
     }
 }
--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,12 +1,54 @@
 import { useLoaderData } from "react-router-dom";
+import {useEffect, useState} from "react";
+
+//icon
+import { RxSlash } from "react-icons/rx";
+
+//api
+import {getMedia} from "../api";
 
 export default function Post() {
     const posts = useLoaderData();
-    console.log(posts)
+    //console.log(posts);
+    const [featuredImage, setFeaturedImage] = useState(null);
+
+    function convertDate(dateStr) {
+        const dateObj = new Date(dateStr);
+
+        // Define options to format the date
+        const options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        };
+
+        return dateObj.toLocaleDateString('en-US', options);
+    }
+
+    useEffect(() => {
+        if(posts.featured_media) {
+            getMedia(posts.featured_media).then((media) => {
+                setFeaturedImage(media.guid.rendered);
+            })
+        }
+    },[posts.featured_media]);
+
 
     return (
         <>
-            <div dangerouslySetInnerHTML={{ __html: posts.content.rendered }}></div>
+            <div className="max-w-screen-xl py-[120px] px-5 xl:px-0 mx-auto">
+                <div>
+                    <img src={featuredImage} alt={posts.title.rendered}/>
+                    <h2 className="text-[24px] md:text-[30px] font-medium pt-[25px] mb-[15px]"
+                        dangerouslySetInnerHTML={{__html: posts.title.rendered}}></h2>
+                    <div>
+                       <p>{convertDate(posts.date)}</p>
+                        <RxSlash />
+                    </div>
+                </div>
+                <div className="max-w-full prose prose-p:max-w-full prose-p:md:max-w-[810px] prose-p:mx-auto"
+                     dangerouslySetInnerHTML={{__html: posts.content.rendered}}></div>
+            </div>
         </>
     )
 }
